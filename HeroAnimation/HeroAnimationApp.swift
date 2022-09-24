1d3b0a1811046ae3d95d626188ca91f354319b9a
Juan Camilo Marín Ochoa <99774186+jcamilo1599@users.noreply.github.com>
2022-09-24 01:11:17 -0500

    Initial Commit

---
+++ HeroAnimation/HeroAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  HeroAnimationApp.swift
+//  HeroAnimation
+//
+//  Created by Juan Camilo Marín Ochoa on 24/09/22.
+//
+
+import SwiftUI
+
+@main
+struct HeroAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
